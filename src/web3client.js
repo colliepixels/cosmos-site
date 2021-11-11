5fd7feb81ed314bc901d8903e090c671fee620d4
colliepixels <84257851+colliepixels@users.noreply.github.com>
2021-11-11 12:15:48 -0700

    Add files via upload

---
+++ src/web3client.js
@@ -0,0 +1,28 @@
+import Web3 from 'web3';
+
+let selectedAccount;
+
+export const init = async () => {
+	let provider = window.ethereum;
+
+	if (typeof provider !== 'undefined') {
+		provider
+			.request({ method: 'eth_requestAccounts' })
+			.then((accounts) => {
+				selectedAccount = accounts[0];
+				console.log(`Selected account is ${selectedAccount}`);
+			})
+			.catch((err) => {
+				console.log(err);
+				return;
+			});
+
+		window.ethereum.on('accountsChanged', function (accounts) {
+			selectedAccount = accounts[0];
+			console.log(`Selected account changed to ${selectedAccount}`);
+		});
+	}
+    const web3 = new Web3(provider);
+
+    const networkId = await web3.eth.net.getId();
+}
